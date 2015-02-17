78cab1a25b77efd63ca2564899fde63bb4168b16
Vad1mo <Vad1mo@users.noreply.github.com>
2015-02-17 15:16:10 +0100

    initial ZUGFeRD .NET Example

---
+++ KonikNetExample/NetExampleInvoice.cs
@@ -0,0 +1,108 @@
+﻿/* Copyright (C) 2014 konik.io
+ *
+ * This file is part of the Konik library.
+ *
+ * The Konik library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The Konik library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with the Konik library. If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+
+using java.io;
+
+using org.apache.commons.lang3.time;
+using com.neovisionaries.i18n;
+
+using io.konik;
+using io.konik.zugferd;
+using io.konik.zugferd.entity;
+using io.konik.zugferd.entity.trade;
+using io.konik.zugferd.entity.trade.item;
+using io.konik.zugferd.profile;
+using io.konik.zugferd.unece.codes;
+using io.konik.zugferd.unqualified;
+
+using Country = com.neovisionaries.i18n.CountryCode;
+using Currency = com.neovisionaries.i18n.CurrencyCode;
+
+
+
+namespace io.konik.net.example
+{
+	class NetExampleInvoice
+	{
+		public static void Main (string[] args)
+		{
+			System.Console.WriteLine ("Konik for .NET");
+			Invoice invoice = createInvoice ();
+			attachInvoiceToPdf (invoice);
+			System.Console.WriteLine ("ZUGFeRD Invocie created see outputfile acme_invoice-42_ZUGFeRD.pdf");
+		}
+
+		public static Invoice createInvoice ()
+		{
+
+			ZfDate today = new ZfDateDay ();
+			ZfDate nextMonth = new ZfDateMonth (DateUtils.addMonths (today, 1));
+
+			Invoice invoice = new Invoice (ConformanceLevel.BASIC);    // <1>
+			invoice.setHeader (new Header ()
+				.setInvoiceNumber ("20131122-42")
+				.setCode (DocumentCode._380)
+				.setIssued (today)
+				.addNote (new Note ("MFG"))
+				.setName ("Rechnung"));
+
+			Trade trade = new Trade ();
+			trade.setAgreement (new Agreement ()    // <2>
+				.setSeller (new TradeParty ()
+					.setName ("Seller Inc.")
+					.setAddress (new Address ("80331", "Marienplatz 1", "München", Country.DE))
+					.addTaxRegistrations (new TaxRegistration ("DE122...", Reference.FC)))
+				.setBuyer (new TradeParty ()
+					.setName ("Buyer Inc.")
+					.setAddress (new Address ("50667", "Domkloster 4", "Köln", Country.DE))
+					.addTaxRegistrations (new TaxRegistration ("DE123...", Reference.FC))));
+
+			trade.setDelivery (new Delivery (nextMonth));
+
+			trade.setSettlement (new Settlement ()
+				.setPaymentReference ("20131122-42")
+				.setCurrency (Currency.EUR)
+				.addPaymentMeans (new PaymentMeans ()
+					.setPayeeAccount (new CreditorFinancialAccount ("DE01234.."))
+					.setPayeeInstitution (new FinancialInstitution ("GENO...")))
+				.setMonetarySummation (new MonetarySummation ()
+					.setLineTotal (new Amount (100, Currency.EUR))
+					.setChargeTotal (new Amount (0, Currency.EUR))
+					.setAllowanceTotal (new Amount (0, Currency.EUR))
+					.setTaxBasisTotal (new Amount (100, Currency.EUR))
+					.setTaxTotal (new Amount (19, Currency.EUR))               
+					.setGrandTotal (new Amount (119, Currency.EUR))));
+
+			trade.addItem (new Item ()
+				.setProduct (new Product ().setName ("Saddle"))
+				.setDelivery (new SpecifiedDelivery (new Quantity (1, UnitOfMeasurement.UNIT))));
+			invoice.setTrade (trade);
+
+			return invoice;
+		}
+
+		static void attachInvoiceToPdf (Invoice invoice)
+		{
+			PdfHandler handler = new PdfHandler ();    // <1>
+			InputStream inputPdf = new FileInputStream ("../../resources/acme_invoice-42.pdf");
+			OutputStream resultingPdf = new FileOutputStream ("acme_invoice-42_ZUGFeRD.pdf");
+			handler.appendInvoice (invoice, inputPdf, resultingPdf);     // <2>
+		}
+	}
+}
---
+++ KonikNetExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("KonikNetExample")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Bauer Engineering")]
+[assembly: AssemblyProduct ("Konik .NET Example")]
+[assembly: AssemblyCopyright ("Konik.io")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.0")]
